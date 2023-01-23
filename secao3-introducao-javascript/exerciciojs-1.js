d28c07dabb2add4b8dd049e9d0848f3709d4dcb9
Milena Cordeiro <mcs9823@gmail.com>
2023-01-23 19:16:34 -0300

    escrevi códigos utilizando as operações aritimeticas básica+, -, *, / e %

---
+++ secao3-introducao-javascript/exerciciojs-1.js
@@ -0,0 +1,17 @@
+const a = 10;
+const b = 5;
+
+let soma = a + b;
+console.log(soma)
+
+let sub = a - b;
+console.log(sub)
+
+let mult = a * b;
+console.log(mult)
+
+let divi = a / b;
+console.log(divi)
+
+let mod = a % b;
+console.log(mod)
