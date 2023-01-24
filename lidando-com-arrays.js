b04558559922ec10191f39715269b6bdd81d4106
Milena Cordeiro <mcs9823@gmail.com>
2023-01-24 19:37:48 -0300

    calculando a média dos valores contidos na array

--- lidando-com-arrays.js
+++ lidando-com-arrays.js
@@ -1,12 +1,18 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let result = 0;
+//let result = 0;
+let soma = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
    // console.log(numbers[index])
 }
 
-for (let index = 0; index < numbers.length; index += 1) {
-    result += numbers[index];
+for (let index = 0; index < numbers.length; index += 1) { // soma todos os números dentro da Array
+   // result += numbers[index];
+      soma += numbers[index]; 
 }
 
-console.log (result)
+//console.log (result)
+
+let media = soma / numbers.length
+
+console.log (media)
