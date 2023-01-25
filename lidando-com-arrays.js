9279f898e2a273dca50449f7faccf4bb793d14c3
Milena Cordeiro <mcs9823@gmail.com>
2023-01-25 10:18:55 -0300

    utilizando for para criar um novo array que vá de 1 até 25

--- lidando-com-arrays.js
+++ lidando-com-arrays.js
@@ -5,7 +5,8 @@ let soma = 0;
 for (let index = 0; index < numbers.length; index += 1) {
    // console.log(numbers[index])
 }
-
+//Calcule e imprima a média aritmética dos valores contidos no array (result+=numbers[index])
+//Some todos os valores contidos no array e imprima o resultado (soma+=numbers[index])
 for (let index = 0; index < numbers.length; index += 1) { // soma todos os números dentro da Array
    // result += numbers[index];
       soma += numbers[index]; 
@@ -17,12 +18,14 @@ let media = soma / numbers.length
 
 //console.log (media)
 
+//Com base no código que acima, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
 if (media > 20) {
    //console.log('valor maior que 20')
 } else {
    //console.log ('Valor menor ou igual a 20')
 }
 
+//Utilizando for, descubra qual o maior valor contido no array e imprima-o;
 let maiorNumero = numbers[0];
 
 for(index = 0; index < numbers.length; index += 1) {
@@ -32,14 +35,33 @@ for(index = 0; index < numbers.length; index += 1) {
    }
 }
 
+//Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
 let numeroImpar = numbers[0]
 
 for(index = 0; index < numbers.length; index += 1) {
    if((numbers[index] % 2) !== 0) {
   numeroImpar = numbers[index]
-  console.log(numeroImpar)
+  //console.log(numeroImpar)
 } else {
-   console.log('nenhum valor impar encontrado')
+   //console.log('nenhum valor impar encontrado')
 }
 }
+
+//Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+
+let numeroMenor = numbers[0]
+
+for (index = 0; index < numbers.length; index += 1) {
+   if (numbers[index] < numeroMenor) {
+      numeroMenor = numbers[index]
+      //console.log(numeroMenor)
+   }
+}
+
+//Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+let novoArray = []
+for (let index = 0; index <= 25; index += 1) {
+   novoArray.push(index)
+}
+console.log(novoArray)
 
