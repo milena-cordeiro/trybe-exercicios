2473b8a387b776780530c0361509d06e1f887821
Milena Cordeiro <mcs9823@gmail.com>
2023-01-26 09:34:06 -0300

    utilizando for para inverter palavras

---
+++ dia-3-JS-logica-programação-algoritimo/inverter-string.js
@@ -0,0 +1,11 @@
+//Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+//como inverter a palavra? posso decrementar -= 1?
+
+let word = 'tryber';
+let inverterWord = '';
+
+for (let index = word.length - 1; index >= 0; index -= 1) {
+ inverterWord += word[index];
+}
+
+console.log(inverterWord)
