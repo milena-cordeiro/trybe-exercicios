37f681d5e5547c3e170a549a02ab2012d6290a6a
Milena Cordeiro <mcs9823@gmail.com>
2023-02-06 11:22:41 -0300

    tentando realizar exercicio da seção 4 dia 04 - exercicio: aplicando o DOM

---
+++ seção-4-JS-DOM-Eventos-WebStorage/script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+   
+    const teste = () => {
+
+        document.body.style.backgroundColor = 'White';
+    }
+
+    btnFundo = document.querySelector('.btn-fundo1');
+    btnFundo.addEventListener('click',teste);
+
+}
