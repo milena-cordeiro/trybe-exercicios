aa474c9555a38ea656e049777b9854377bcc3cb0
Milena Cordeiro <mcs9823@gmail.com>
2023-03-02 20:30:10 -0300

    Função createEmployee

--- fundamentos/secao-7-Higher-Order-Functions/Dia 01 - Introducao-a-HOFs/functions.js
+++ fundamentos/secao-7-Higher-Order-Functions/Dia 01 - Introducao-a-HOFs/functions.js
@@ -1,8 +1,21 @@
 // Parte I
 // Nova pessoa contratada
-// Você ficou responsável por organizar o sistema de contratação de uma empresa e deve criar uma estrutura de dados que possua o formato { nomeCompleto, email } para representar a nova pessoa contratada. Além disso, você deve gerar os emails de cada pessoa que é contratada na empresa. Para isso:
+// Você ficou responsável por organizar o sistema de contratação de uma empresa e deve criar uma estrutura de dados que possua o formato { nomeCompleto, email } para representar a nova pessoa contratada. Além disso, você deve gerar os emails de cada pessoa que é contratada na empresa. 
+
 
 // crie uma função que receba como parâmetro o nome completo da nova pessoa colaboradora e que automaticamente gere um email no formato nome_da_pessoa@trybe.com;
+const criaEmail = (fullName) => {
+const email = fullName.toLowerCase().replace(' ', '_');
+return {fullName, email: `${email}@trybe.com`};
 
-// utilize a função newEmployees, definida abaixo, para receber a sua função como parâmetro e criar o objeto de cada pessoa contratada.
+};
 
+const newEmployees = (callBack) => {
+  const employees = {
+    id1: callBack('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callBack('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callBack('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+console.log(newEmployees(criaEmail));
