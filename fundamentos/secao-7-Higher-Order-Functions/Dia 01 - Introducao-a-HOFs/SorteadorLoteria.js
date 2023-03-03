9de977aa872ded2b6c6eb722a60a8031ec67b3ca
Milena Cordeiro <mcs9823@gmail.com>
2023-03-03 09:17:46 -0300

    alguns ajustes

--- fundamentos/secao-7-Higher-Order-Functions/Dia 01 - Introducao-a-HOFs/SorteadorLoteria.js
+++ fundamentos/secao-7-Higher-Order-Functions/Dia 01 - Introducao-a-HOFs/SorteadorLoteria.js
@@ -1,6 +1,7 @@
 // Sorteador de loteria
 // Desenvolva uma HOF que retorne o resultado de um sorteio. Essa HOF irá gerar um número aleatório entre 1 e 5, recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: “Tente novamente” ou “Parabéns, você ganhou”).
-const checkNumero = (myNumber, numSorteado) => myNumber === numSorteado; //1. criar função de callback que verifica se os numerossão iguais
+ //1. criar função de callback que verifica se os numeros são iguais
+ const checkNumero = (myNumber, numSorteado) => myNumber === numSorteado;
 // 2. cria a função que recebera a callBack de parametro e um numero,
 // ela irá gerar um numero aleatório e retornar a função calback e o resultado da condição se ela for true ou false
 const resultSorteio = (myNumber, callBack) => {
