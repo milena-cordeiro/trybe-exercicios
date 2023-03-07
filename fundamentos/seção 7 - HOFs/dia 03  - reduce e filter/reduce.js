d1f9c0aee98a9e486504ea5531872177c9945225
Milena Cordeiro <mcs9823@gmail.com>
2023-03-07 15:07:01 -0300

    Ex 5 - retornando um array de objetos com nome do studant e media de notas

--- fundamentos/seção 7 - HOFs/dia 03  - reduce e filter/reduce.js
+++ fundamentos/seção 7 - HOFs/dia 03  - reduce e filter/reduce.js
@@ -111,5 +111,20 @@ const arrayLetras = names.join('').toLowerCase().split('');
 return arrayLetras.reduce((acc, curr) => { return curr === 'a' ? acc += 1: acc;}, 0);
 
 }
+// console.log(countA());
 
-console.log(countA());
+// 5 - Crie um array de objetos e calcule a média de notas. Utilize as constantes students e grades para criar um array de objetos e calcule a média da nota das pessoas estudantes:
+// O index 0 do array `students` equivale ao index 0 do array `grades`
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+const expectedResult = [
+ { name: 'Pedro Henrique', average: 7.8 },
+ { name: 'Miguel', average: 9.2 },
+ { name: 'Maria Clara', average: 8.8 },
+];
+const studentAverage = (studantsList, gradesList) => {
+ return studantsList.map((studant, i) => ({name: studant,
+  average: (gradesList[i].reduce((acc, curr) => acc + curr, 0) / gradesList[i].length)}));
+};
+
+console.log(studentAverage(students, grades));
