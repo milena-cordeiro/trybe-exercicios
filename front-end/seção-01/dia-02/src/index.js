e240472d2dd29e8ec73affe3a2f26e8f6e37fc2f
Milena Cordeiro <mcs9823@gmail.com>
2023-03-18 13:09:18 -0300

    finalizando

--- front-end/seção-01/dia-02/src/index.js
+++ front-end/seção-01/dia-02/src/index.js
@@ -1,13 +1,25 @@
 import Swal from 'sweetalert2'
+import './style.css';
 
-const img = document.querySelector('#heroImg')
-const heroName = document.querySelector('heroName');
+const imgHero = document.querySelector('#heroImg')
+const heroName = document.querySelector('.heroName');
 const tokenAccess = '6223268867733506'
 const btn = document.querySelector('button');
-let BASE_URL = `https://www.superheroapi.com/api/${tokenAccess}`;
+let BASE_URL = `https://www.superheroapi.com/api.php/${tokenAccess}`;
 
 btn.addEventListener('click', () => {
   const randomId = Math.round(Math.random() * 1000);
   const id = randomId;
-  fetch(BASE_URL).then((res) => console.log(res))
+  fetch(`${BASE_URL}/${id}`).then((res) => res.json()).then((data) => {
+    const { name , image } = data;
+    imgHero.src = image.url;
+    heroName.innerHTML = name;
+  }).catch((e) => {
+    Swal.fire({
+      title: 'Hero not found',
+      text: e.message,
+      icon: 'error',
+      confirmButtonText: 'Cool'
+    })
+  })
 });
